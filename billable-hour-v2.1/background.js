438d4f07c14d45cfa1c0aa74a8d6a27df6f228d4
Peng, Yifeng <ypeng.yvonne@gmail.com>
2026-05-06 22:51:20 -0500

    remove unnecessary files

--- billable-hour-v2.1/background.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.windows.create({
-    url: chrome.runtime.getURL("window-full.html"),
-    type: "popup",
-    width: 450,
-    height: 660,
-    focused: true
-  });
-});
--- billable-hour-v2/background.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.windows.create({
-    url: chrome.runtime.getURL("window.html"),
-    type: "popup",
-    width: 420,
-    height: 600,
-    focused: true
-  });
-});
